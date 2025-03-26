0c635f19f5859da3459f7c7862c9a1f6ea717db3
Seth <shova.seth@capgemini.com>
2025-03-26 12:58:53 +0530

    comment added

--- Backend/routes/cartRoutes.js
+++ Backend/routes/cartRoutes.js
@@ -3,7 +3,7 @@ const cart= require('../controllers/cartController');
 
 const router = express.Router();
 
-router.post('/add', cart.addToCart);
+router.post('/add', cart.addToCart); //add cart
 router.get('/:userId', cart.getCartItems);
 router.delete('/:userId/:productId', cart.deleteItem);
 router.delete('/:userId', cart.clearCart);
